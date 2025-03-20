4f8bb4cda311de92eb7bb3ce1bde50510c3df279
tsgamage <thevidu2007627@gmail.com>
2025-03-20 13:09:25 +0530

    Handle get request and render home page

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-import express from "express"
-import axios from "axios"
+import express from "express";
+import axios from "axios";
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-app.listen(PORT,()=>{
-    console.log("Listening to port ", PORT)
-})
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    secret: "none",
+    user: "none",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Listening to port ", PORT);
+});
