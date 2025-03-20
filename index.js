0cffc7f167a033cec6f8114b6780ca333cc9595a
tsgamage <thevidu2007627@gmail.com>
2025-03-20 13:19:23 +0530

    Get data from API and Send data to home page

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import express from "express";
 import axios from "axios";
+import dotenv from "dotenv";
 
 const app = express();
 const PORT = 3000;
-
 app.use(express.static("public"));
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    secret: "none",
-    user: "none",
-  });
+const API_URL = process.env.API_URL;
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL + "random");
+    const responseSecret = response.data.secret;
+    const responseUser = response.data.username;
+    res.render("index.ejs", {
+      secret: responseSecret,
+      user: responseUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("index.ejs", {
+      secret: "Error",
+      user: "Error while getting data",
+    });
+  }
 });
 
 app.listen(PORT, () => {
